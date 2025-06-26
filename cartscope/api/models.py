b74ee30065104c433069709b4613709395d1ce09
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-26 09:47:05 +0530

    List Of Models To Be Provided Decided

--- cartscope/api/models.py
+++ cartscope/api/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+class ProductCategory(models.Model):
+    pass
+class Sellers(models.Model):
+    pass
+class Product(models.Model):
+    pass
+class ProductReviews(models.Model):
+    pass
+class Cart(models.Model):
+    pass
+class Order(models.Model):
+    pass
