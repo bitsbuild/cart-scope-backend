882ea195f7e91a3ab9b53f2af899932de07eda39
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-26 09:50:10 +0530

    Models Registered At Admin Site

--- cartscope/api/admin.py
+++ cartscope/api/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from api.models import Cart,Product,ProductCategory,ProductReviews,Order,Sellers
+admin.site.register(Cart)
+admin.site.register(Product)
+admin.site.register(ProductCategory)
+admin.site.register(ProductReviews)
+admin.site.register(Order)
+admin.site.register(Sellers)
