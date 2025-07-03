762da9454b7ae8fca2d560df59bbf44041ec43e6
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-03 21:45:14 +0530

    Practice Practice Practice

--- cartscope/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from api.models import Cart,Product,ProductCategory,ProductReview,Order,Seller
-admin.site.register(Cart)
-admin.site.register(Product)
-admin.site.register(ProductCategory)
-admin.site.register(ProductReview)
-admin.site.register(Order)
-admin.site.register(Seller)
