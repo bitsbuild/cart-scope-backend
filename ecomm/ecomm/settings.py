a14c851a67f1c83cf1e76f8e1f6e1c5f45a81701
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-05 23:58:10 +0530

    FSC#4: Rest Framework Added To The Settings File, User App Registered In Settings File, User Serializer Created

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -10,6 +10,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'user'
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
