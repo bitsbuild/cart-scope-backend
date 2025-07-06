b9ccb0326eb2e53c1594ec950e0d84858e907c93
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-06 15:47:36 +0530

    FSC#18: Error In Get Toekn Url Defintion Fixed

--- ecomm/user/urls.py
+++ ecomm/user/urls.py
@@ -3,6 +3,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 from user.views import create_user,delete_user
 urlpatterns = [
     path('create/',create_user,name='create'),
-    path("gettoken/",obtain_auth_token,'gettoken'),
+    path("gettoken/",obtain_auth_token,name='gettoken'),
     path("delete/",delete_user,name='delete')
 ]
