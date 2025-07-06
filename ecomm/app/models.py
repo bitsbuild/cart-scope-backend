53f709d3461189981a30a50ef7e26bae9e5c52c5
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-06 23:42:18 +0530

    FSC#27: Product-Seller Ecosystem Taking Up Shape

--- ecomm/app/models.py
+++ ecomm/app/models.py
@@ -1,8 +1,29 @@
-from django.db import models
+from django.db.models import (
+    Model,
+    UUIDField,
+    CharField,
+    TextField,
+    DateTimeField,
+    ForeignKey,
+    CASCADE,
+    IntegerField,
+    BigIntegerField
+)
 import uuid
-class Product(models.Model):
-    id = models.UUIDField(primary_key=True,read_only=True,default=uuid.uuid4)
-    name = models.CharField()
-    about = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+class Seller(Model):
+    id = UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    name = CharField()
+    about = TextField()
+    location = TextField()
+    postal_code = BigIntegerField()
+    created = DateTimeField(auto_now_add=True,editable=False)
+    updated = DateTimeField(auto_now=True,editable=False)
+class Product(Model):
+    id = UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
+    name = CharField()
+    about = TextField()
+    amount = IntegerField()
+    price = IntegerField()
+    created = DateTimeField(auto_now_add=True,editable=False)
+    updated = DateTimeField(auto_now=True,editable=False)
+    seller = ForeignKey(Seller,related_name='inventory',on_delete=CASCADE)
