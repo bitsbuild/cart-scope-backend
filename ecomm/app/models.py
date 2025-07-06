d75cc1bb5f77d3c243a5a53b64ee70f89f2e7777
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-06 19:34:38 +0530

    FSC#26: Basics OF Product Model Written

--- ecomm/app/models.py
+++ ecomm/app/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+import uuid
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True,read_only=True,default=uuid.uuid4)
+    name = models.CharField()
+    about = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
