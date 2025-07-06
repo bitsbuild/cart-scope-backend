d03c7f9f2312ecf39797cf8367a170c3cc612a67
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-06 12:01:51 +0530

    FSC#10: Test Functions Code Structure Defined

--- ecomm/user/tests.py
+++ ecomm/user/tests.py
@@ -1,1 +1,21 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+class UserTesting(APITestCase):
+    def setUp(self):
+        pass
+    def test_create_user_success(self):
+        pass
+    def test_create_user_failure_passwords_not_matching(self):
+        pass
+    def test_create_user_failure_username_exists(self):
+        pass
+    def test_create_user_failure_account_with_email_exists(self):
+        pass
+    def test_get_token_success(self):
+        pass
+    def test_get_token_failure(self):
+        pass
+    def delete_user_success(self):
+        pass
+    def delete_user_failure(self):
+        pass
