0b2e9fe6d3929c155c315527e3fa2c790dbc7407
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-06 12:19:06 +0530

    FSC#12: Create User Success Test Case Written

--- ecomm/user/tests.py
+++ ecomm/user/tests.py
@@ -1,10 +1,18 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
+from rest_framework.status import HTTP_200_OK,HTTP_400_BAD_REQUEST
 class UserTesting(APITestCase):
     def setUp(self):
         pass
     def test_create_user_success(self):
-        pass
+        data = {
+            "username":"bitsbuild",
+            "password":"password",
+            "email":"email",
+            "confirm_password":"password"
+        }
+        response = self.client.post(reverse('create'),data)
+        self.assertEqual(response.status_code,HTTP_200_OK)
     def test_create_user_failure_passwords_not_matching(self):
         pass
     def test_create_user_failure_username_exists(self):
