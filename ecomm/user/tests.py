98cf81e871c6f2c778578adbe88275d0bd31db82
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-06 17:46:04 +0530

    FSC#21: Unit Testing For User App Completed

--- ecomm/user/tests.py
+++ ecomm/user/tests.py
@@ -1,7 +1,8 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
-from rest_framework.status import HTTP_200_OK,HTTP_400_BAD_REQUEST
+from rest_framework.authtoken.models import Token
+from rest_framework.status import HTTP_200_OK,HTTP_400_BAD_REQUEST,HTTP_401_UNAUTHORIZED
 class UserTesting(APITestCase):
     def setUp(self):
         User.objects.create_user(username="bitsbuild1",email="email1@email1.com",password="password")
@@ -51,10 +52,27 @@ class UserTesting(APITestCase):
         response = self.client.post(reverse('create'),data)
         self.assertEqual(response.status_code,HTTP_400_BAD_REQUEST)
     def test_get_token_success(self):
-        pass
+        data = {
+            "username":"bitsbuild1",
+            "password":"password"
+        }
+        user = User.objects.filter(username="bitsbuild1").first()
+        token= Token.objects.get(user=user)
+        response = self.client.post(reverse('gettoken'),data)
+        self.assertEqual(response.data['token'],token.key)
     def test_get_token_failure(self):
-        pass
+        data = {
+            "username":"bitsbuild1",
+            "password":"password1"
+        }
+        response = self.client.post(reverse('gettoken'),data)
+        self.assertEqual(response.status_code,HTTP_400_BAD_REQUEST)
     def test_delete_user_success(self):
-        pass
+        token = Token.objects.get(user__username="bitsbuild1")
+        self.client.credentials(HTTP_AUTHORIZATION='Token '+str(token.key))
+        response = self.client.delete(reverse('delete'))
+        self.assertEqual(response.status_code,HTTP_200_OK)
     def test_delete_user_failure(self):
-        pass
+        self.client.credentials(HTTP_AUTHORIZATION='Token '+'')
+        response = self.client.delete(reverse('delete'))
+        self.assertEqual(response.status_code,HTTP_401_UNAUTHORIZED)
