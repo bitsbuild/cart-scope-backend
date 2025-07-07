0aae3ba8bfc9bb237e6c28144ae0e6e1dce5c93e
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-07 23:57:58 +0530

    FSC#39: Models Registed To Admin Site So They Can Be Managed From Admin Site

--- ecomm/app/admin.py
+++ ecomm/app/admin.py
@@ -1,1 +1,5 @@
 from django.contrib import admin
+from app.models import Seller,ProductCategory,Product
+admin.site.register(Seller)
+admin.site.register(ProductCategory)
+admin.site.register(Product)
