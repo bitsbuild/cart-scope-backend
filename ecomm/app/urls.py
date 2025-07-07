f4466fb355a9259cb23ee2399babb4a9ddd362f3
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-07 23:53:33 +0530

    FSC#37: URLs File Written For Existing Models As Of This Point In Time

--- ecomm/app/urls.py
+++ ecomm/app/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path
-urlpatterns = [
-    
-]
+from rest_framework.routers import DefaultRouter
+from app.views import SellerViewSet,ProductCategoryViewSet,ProductViewSet
+router = DefaultRouter()
+router.register(r'seller',SellerViewSet,basename='seller')
+router.register(r'category',ProductCategoryViewSet,basename='category')
+router.register(r'product',ProductViewSet,basename='product')
