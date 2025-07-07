ccefea3b50a896e3b795f5381b2a85145cd5da59
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-07 23:05:05 +0530

    FSC#34: Serializers Written Individual Field Level Specifications Will Be Added On Testing Basis

--- ecomm/app/serializers.py
+++ ecomm/app/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 from app.models import Seller,ProductCategory,Product
 class SellerSerializer(ModelSerializer):
-    pass
+    class Meta:
+        model = Seller
+        fields = '__all__'
 class ProductCategorySerializer(ModelSerializer):
-    pass
+    class Meta:
+        model = ProductCategory
+        fields = '__all__'
 class ProductSerializer(ModelSerializer):
-    pass
+    class Meta:
+        model = Product
+        fields = '__all__'
