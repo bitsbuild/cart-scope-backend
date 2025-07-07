eb99355929e0deba0b4b303f26292bc7ae7d9892
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-07 22:51:40 +0530

    FSC#30: STR Functions Added To Models And Premium Quality Seller Option Field Created And Migrations Applied

--- ecomm/app/models.py
+++ ecomm/app/models.py
@@ -16,15 +16,20 @@ import uuid
 class ProductCategory(Model):
     id = UUIDField(primary_key=True,editable=False,default=uuid.uuid4,blank=False)
     name = CharField(max_length=700,blank=False)
+    def __str__(self):
+        return self.name
 class Seller(Model):
     id = UUIDField(primary_key=True,default=uuid.uuid4,editable=False,blank=False)
     name = CharField(max_length=700,blank=False)
     about = TextField(max_length=3500,blank=False)
     postal_code = BigIntegerField(blank=False)
     location = TextField(blank=False)
-    rating = FloatField(validators=[MinValueValidator(0),MaxValueValidator(5)],default=0,blank=False)
+    is_premium_seller = BooleanField(default=False,blank=False,editable=False)
+    rating = FloatField(validators=[MinValueValidator(0),MaxValueValidator(5)],default=0,blank=False,editable=False)
     created = DateTimeField(auto_now_add=True,editable=False,blank=False)
     updated = DateTimeField(auto_now=True,editable=False,blank=False)
+    def __str__(self):
+        return self.name
 class Product(Model):
     id = UUIDField(primary_key=True,editable=False,default=uuid.uuid4,blank=False)
     name = CharField(max_length=700,blank=False)
@@ -33,6 +38,8 @@ class Product(Model):
     seller = ForeignKey(Seller,related_name='inventory',on_delete=CASCADE,blank=False)
     quantity = IntegerField(blank=False)
     price = IntegerField(blank=False)
-    rating = FloatField(validators=[MinValueValidator(0),MaxValueValidator(5)],default=0,blank=False)
+    rating = FloatField(validators=[MinValueValidator(0),MaxValueValidator(5)],default=0,blank=False,editable=False)
     created = DateTimeField(auto_now_add=True,editable=False,blank=False)
     updated = DateTimeField(auto_now=True,editable=False,blank=False)
+    def __str__(self):
+        return self.name
