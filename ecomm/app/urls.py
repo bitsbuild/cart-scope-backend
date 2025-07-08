9c93725754a5ce9db1364eff16c9865c8f479229
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-08 13:13:35 +0530

    FSC#53: URLs For Product Images Registered To Router

--- ecomm/app/urls.py
+++ ecomm/app/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from app.views import SellerViewSet,ProductCategoryViewSet,ProductViewSet
+from app.views import SellerViewSet,ProductCategoryViewSet,ProductViewSet,ProductImagesViewSet
 router = DefaultRouter()
 router.register(r'seller',SellerViewSet,basename='seller')
 router.register(r'category',ProductCategoryViewSet,basename='category')
 router.register(r'product',ProductViewSet,basename='product')
+router.register(r'productimages',ProductImagesViewSet,basename='productimages')
 urlpatterns = router.urls
