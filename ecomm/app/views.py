d7cdb4ee4930f76005f669a9a3e6602cc56f3f21
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-08 13:12:09 +0530

    FSC#52: Views Written For Product Images

--- ecomm/app/views.py
+++ ecomm/app/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
-from app.models import Seller,ProductCategory,Product
-from app.serializers import SellerSerializer,ProductCategorySerializer,ProductSerializer
+from app.models import Seller,ProductCategory,Product,ProductImages
+from app.serializers import SellerSerializer,ProductCategorySerializer,ProductSerializer,ProductImagesSerializer
 class SellerViewSet(ModelViewSet):
     queryset = Seller.objects.all()
     serializer_class = SellerSerializer
@@ -10,3 +10,6 @@ class ProductCategoryViewSet(ModelViewSet):
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+class ProductImagesViewSet(ModelViewSet):
+    queryset=ProductImages.objects.all()
+    serializer_class=ProductImagesSerializer
