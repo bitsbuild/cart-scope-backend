30ad377816b70be28060ff58c68eba9be72e9730
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-08 16:56:45 +0530

    FSC#56: Product Images Model Registered To Admin Site

--- ecomm/app/admin.py
+++ ecomm/app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from app.models import Seller,ProductCategory,Product
+from app.models import Seller,ProductCategory,Product,ProductImages
 admin.site.register(Seller)
 admin.site.register(ProductCategory)
 admin.site.register(Product)
+admin.site.register(ProductImages)
