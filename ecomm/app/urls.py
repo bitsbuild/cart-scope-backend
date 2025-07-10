cf83cd67014a5396777a9f700bd40a30aaba381f
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-10 18:17:02 +0530

    FSC#69: URLs Written, ViewSets Written, Serializers Written, Models Written

--- ecomm/app/urls.py
+++ ecomm/app/urls.py
@@ -1,8 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from app.views import SellerViewSet,ProductCategoryViewSet,ProductViewSet,ProductImagesViewSet
+from app.views import SellerViewSet,ProductCategoryViewSet,ProductViewSet,ProductImagesViewSet,ReviewViewSet,OrderViewSet
 router = DefaultRouter()
 router.register(r'seller',SellerViewSet,basename='seller')
 router.register(r'category',ProductCategoryViewSet,basename='category')
 router.register(r'product',ProductViewSet,basename='product')
 router.register(r'productimages',ProductImagesViewSet,basename='productimages')
+router.register(r'reviews',ReviewViewSet,basename='reviews')
+router.register(r'orders',OrderViewSet,basename='orders')
 urlpatterns = router.urls
