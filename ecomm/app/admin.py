92a9761d20b5aae14f256c33151d4794ef29bb10
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-10 16:50:55 +0530

    Review And Order Models Registered To Admin Site

--- ecomm/app/admin.py
+++ ecomm/app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from app.models import Seller,ProductCategory,Product,ProductImages
+from app.models import Seller,ProductCategory,Product,ProductImages,Review,Order
 admin.site.register(Seller)
 admin.site.register(ProductCategory)
 admin.site.register(Product)
 admin.site.register(ProductImages)
+admin.site.register(Review)
+admin.site.register(Order)
