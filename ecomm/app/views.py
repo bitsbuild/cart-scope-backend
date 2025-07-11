4436632a274f36290eccd3f180b2b4e9cf26a8a8
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-11 23:16:49 +0530

    Errors Found During Testing Fixed, Wrong Field Was Accessed To Get List Of Reviews, No Response Was Returned As Custom Create Function Needs Returning Of Response, Both Issues Fixed With Proper Status Codes

--- ecomm/app/views.py
+++ ecomm/app/views.py
@@ -2,6 +2,8 @@ from rest_framework.viewsets import ModelViewSet
 from app.models import Seller,ProductCategory,Product,ProductImages,Review,Order
 from app.serializers import SellerSerializer,ProductCategorySerializer,ProductSerializer,ProductImagesSerializer,ReviewSerializer,OrderSerializer
 import statistics
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK,HTTP_400_BAD_REQUEST
 class SellerViewSet(ModelViewSet):
     queryset = Seller.objects.all()
     serializer_class = SellerSerializer
@@ -18,15 +20,30 @@ class ReviewViewSet(ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        product = request.data['product']
-        reviews = list(Review.objects.filter(id=product).values_list('stars',flat=True))
-        rating_product = statistics.mean(reviews)
-        product_review_change = Product.objects.get(pk=product)
-        product_review_change.rating = rating_product
-        product_review_change.save()
+        try:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            product = request.data['product']
+            reviews = list(Review.objects.filter(product=product).values_list('stars',flat=True))
+            rating_product = statistics.mean(reviews)
+            product_review_change = Product.objects.get(pk=product)
+            product_review_change.rating = rating_product
+            product_review_change.save()
+            return Response(
+                {
+                "Status":"Successfully Added The Review And The Rating (stars) Reflects In Prodct Rating",
+                },
+                status=HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                {
+                    "Status":"Failure To Create Review",
+                    "Error":str(e)
+                },
+                status=HTTP_400_BAD_REQUEST
+            )
 class OrderViewSet(ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
