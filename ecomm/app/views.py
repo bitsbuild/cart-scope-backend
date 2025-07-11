81bea436082ed689f08b0404563ecf1720a0f97d
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-11 23:01:22 +0530

    FSC#72: Review Rating -> Product Rating Logic Written, Testing Pending, Testing To Be Done Using Admin Panel And Postman

--- ecomm/app/views.py
+++ ecomm/app/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from app.models import Seller,ProductCategory,Product,ProductImages,Review,Order
 from app.serializers import SellerSerializer,ProductCategorySerializer,ProductSerializer,ProductImagesSerializer,ReviewSerializer,OrderSerializer
+import statistics
 class SellerViewSet(ModelViewSet):
     queryset = Seller.objects.all()
     serializer_class = SellerSerializer
@@ -20,6 +21,12 @@ class ReviewViewSet(ModelViewSet):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
+        product = request.data['product']
+        reviews = list(Review.objects.filter(id=product).values_list('stars',flat=True))
+        rating_product = statistics.mean(reviews)
+        product_review_change = Product.objects.get(pk=product)
+        product_review_change.rating = rating_product
+        product_review_change.save()
 class OrderViewSet(ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
