92dae89e6ad117986b416c4b0d629d045d699afa
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-12 00:40:37 +0530

    FSC#75: Create Function Initialized For Order View Set So That Invoice Generation Can Be Written

--- ecomm/app/views.py
+++ ecomm/app/views.py
@@ -47,3 +47,7 @@ class ReviewViewSet(ModelViewSet):
 class OrderViewSet(ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
