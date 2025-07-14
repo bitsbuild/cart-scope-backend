d344ce7892f2ab339cdf5e6bb2a61637d9c87eea
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-14 06:27:38 +0530

    Billing Completed, Invoice Gneration And Mailing Left To Implement, Then On To Testing

--- ecomm/app/billing.py
+++ ecomm/app/billing.py
@@ -1,7 +1,10 @@
 def billing(product_name_list,product_quantity_list,product_price_list,discount_percentage):
+    amnt = sum(product_price_list)
+    disc = (discount_percentage*amnt)/100
+    final_amnt = amnt - disc
     return {
-        'amount':0,
-        'discount':0,
-        'final_amount':0,
+        'amount':amnt,
+        'discount':disc,
+        'final_amount':final_amnt,
         'invoice':0
     }
