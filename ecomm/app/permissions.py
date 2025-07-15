006b2b88b089550b49e19c967ce5a37c7afc024d
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-15 21:18:23 +0530

    Permissions For Order Endpoints Set

--- ecomm/app/permissions.py
+++ ecomm/app/permissions.py
@@ -16,4 +16,12 @@ class ReviewPermissions(BasePermission):
             else:
                 return False
 class OrderPermissions(BasePermission):
-    pass
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return True
+    def has_object_permission(self, request, view, obj):
+        if request.method == 'GET':
+            if request.user.is_staff or request.user.is_superuser:
+                return True
+            else:
+                return False
